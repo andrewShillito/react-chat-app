f9359ab25628e17d2fcfa81ad94869f8ffad1358
andrewShillito <andrew.shillito88@gmail.com>
2018-10-24 22:46:29 +0000

    possible code mangling fix

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -3,68 +3,66 @@ import React from "react";
 class ChatWindow extends React.Component {
   state = {
   	inputText: '',
-     
+  }
   isDisabled = () => {
-      his.state.inputText === '') {
+      if (this.state.inputText === '') {
       return true;
     }
-    returnalse;
+    return false;
   }
-  updateInputText (value) => {
-  	this.setState(() => ({
-        tT   value,
-  }))
-  }
-    rInput = () => {
+  updateInputText = (value) => {
     this.setState(() => ({
-      inputText: ''
+      inputText: value,
     }))
   }
-  () {
-  return (
-    <div className="chat-windo>
-    2>Sup Aweme Chat</h2>
-      <div classme="name sender">{this.props.username}</div>
-
-      <ul className="message-list">       {this.props.messages.map((meage, index) => (
-          <li
-            key={index}
-           lasame={
-          mesge.usname === this.props.username ? 'message sender' : 'message recipient'
-              }
-            >
-            <p>{`${message.username}: ${message.text}`}</p>
-          </li>
-        ))}
-      </ul>
-
-      <div>
-      	<form className="input-group">
-      		<input 
-			  type="text" 
-			  className="form-control" 
-			  placeholder="Enter your message..." 
-			  value={this.state.inputText}
-			  onChange=          {(event) => this.updateInputText(event.target.value)}
-			  />
-      		<div className="input-group-append">
-      			<button 
-					className="btn submit-button" 
-					disabled={this.isDisabled()}
-					onClick={(event) => 
-						{	event.preventDefault();
-							let newObj = {username: this.props.username, text: this.state.inputText}
-							this.props.handleAddMessage(newObj)
-							this.clearInput()
-						}}
-					>
-      				SEND
-      			</button>
-      		</div>
-      	</form>
+  clearInput = () => {
+    this.setState(() => ({
+      inputText: '',
+    }))
+  }
+  render() {
+    return (
+      <div className="chat-window">
+        <h2>Super Awesome Chat</h2>
+        <div className="name sender">{this.props.username}</div>
+        <ul className="message-list">
+          {this.props.messages.map((message) => (
+            <li
+              key={index}
+              className={
+                message.username === this.props.username ? 'message sender' : 'message recipient'
+                }
+              >
+              <p>{`${message.username}: ${message.text}`}</p>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <form className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter your message..."
+              value={this.state.inputText}
+              onChange={(event) => this.updateInputText(event.target.value)}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn submit-button"
+                disabled={this.isDisabled()}
+                onClick={(event) => {
+                  event.preventDefault();
+                  let newObj = {username: this.props.username, text: this.state.inputText}
+                  this.props.handleAddMessage(newObj)
+                  this.clearInput()
+                }}
+              >SEND
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-	</div>
-  	)
+    )
   }
 }
 
